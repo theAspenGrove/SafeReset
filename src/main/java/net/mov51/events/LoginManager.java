309c2443c96f2bdfb29877c7250816a627d4377b
mov51 <mov51@mov51.net>
2020-09-30 13:05:31 -0400

    Changed location to use world spawn

--- src/main/java/net/mov51/events/LoginManager.java
+++ src/main/java/net/mov51/events/LoginManager.java
@@ -2,10 +2,17 @@ package net.mov51.events;
 
 import com.earth2me.essentials.User;
 import net.mov51.SafeEnd;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.Objects;
+
+import static org.bukkit.Bukkit.getWorld;
 
 
 public class LoginManager implements Listener {
@@ -16,19 +23,32 @@ public class LoginManager implements Listener {
     public LoginManager(SafeEnd main) {
         this.main = main;
     }
+
     @EventHandler
-    public void PlayerLoginEvent(PlayerLoginEvent e) {
+    public void PlayerJoinEvent(PlayerJoinEvent e) {
         Player p = e.getPlayer();
         User Ip = main.ess.getUser(p);
         String ResetTime = main.getConfig().getString("ResetTime");
-        assert ResetTime != null;
-        System.out.println(ResetTime);
-        System.out.println(Ip.getLastLogout());
-        System.out.println(Long.parseLong(ResetTime));
-        System.out.println("dif=" + (Ip.getLastLogout() < Long.parseLong(ResetTime)));
+        String ResetWorld = main.getConfig().getString("ResetWorld");
 
-        if(Ip.getLastLogout() < Long.parseLong(ResetTime)){
-            System.out.println("HELP");
+        if (ResetTime != null && ResetWorld != null) {
+            System.out.println("Time not null");
+            System.out.println("Config loaded");
+            World World = getWorld(ResetWorld);
+            if (World != null){
+                System.out.println("World is not null" + World.toString());
+                if (p.getWorld() == World) {
+                    System.out.println("Player in world");
+                    if (Ip.getLastLogout() < Long.parseLong(ResetTime)) {
+                        System.out.println("Player outside of time");
+                        Location TpTo = World.getSpawnLocation();
+                        p.teleport(TpTo);
+                    }
+                }
+            }else{
+                System.out.println("World is null");
+            }
+
         }
     }
 }
