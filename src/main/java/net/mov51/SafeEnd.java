f6ab635cb72b9952c6a25a211b49b5328f43238b
James Harrison <james@fasttortoise.co.uk>
2021-02-05 23:50:33 +0000

    Fix code lol

--- src/main/java/net/mov51/SafeEnd.java
+++ src/main/java/net/mov51/SafeEnd.java
@@ -1,15 +1,21 @@
 package net.mov51;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public final class SafeEnd extends JavaPlugin implements Listener {
+
+    //Last Played Hash
+    private HashMap<UUID, Long> hasJoinedBeforeReset = new HashMap<>();
 
     /**
      * When the plugin enables
@@ -26,19 +32,30 @@ public final class SafeEnd extends JavaPlugin implements Listener {
      * @param event
      */
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-
+    public void onAsyncPlayerPreLogin(AsyncPlayerPreLoginEvent event) {
+        OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(event.getPlayerProfile().getId());
         long resetTime = getConfig().getLong("ResetTime");
-        String resetWorld = getConfig().getString("ResetWorld");
+        if (offlinePlayer.getLastSeen() < resetTime) {
+            hasJoinedBeforeReset.put(offlinePlayer.getUniqueId(), System.currentTimeMillis());
+        }
+    }
 
-        if(resetWorld != null) {
-            World world = Bukkit.getWorld(resetWorld);
-            if(world != null && player.getWorld() == world) {
-                if(player.getLastSeen() < resetTime) {
-                    player.teleport(world.getSpawnLocation());
+    /**
+     * On the player join event
+     * @param event
+     */
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Object hasPlayedLastPlayed = hasJoinedBeforeReset.get(event.getPlayer().getUniqueId());
+        if(hasPlayedLastPlayed != null) {
+            String resetWorld = getConfig().getString("ResetWorld");
+            if(resetWorld != null) {
+                World world = Bukkit.getWorld(resetWorld);
+                if(world != null && event.getPlayer().getWorld() == world) {
+                    event.getPlayer().teleport(world.getSpawnLocation());
                 }
             }
         }
+        hasJoinedBeforeReset.remove(event.getPlayer().getUniqueId());
     }
 }
