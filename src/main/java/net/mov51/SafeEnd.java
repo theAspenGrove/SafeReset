5dfc4c5432088779498a0a58cefd47d8b0b13a59
James Harrison <james@fasttortoise.co.uk>
2021-02-05 23:03:48 +0000

    Fixed code

--- src/main/java/net/mov51/SafeEnd.java
+++ src/main/java/net/mov51/SafeEnd.java
@@ -1,23 +1,44 @@
 package net.mov51;
 
-import com.earth2me.essentials.Essentials;
-import net.mov51.events.LoginManager;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SafeEnd extends JavaPlugin{
+public final class SafeEnd extends JavaPlugin implements Listener {
 
-    public Essentials ess = (Essentials) getServer().getPluginManager().getPlugin("Essentials");
-
-
+    /**
+     * When the plugin enables
+     */
     @Override
     public void onEnable() {
         // Plugin startup logic
         this.saveDefaultConfig();
-        getServer().getPluginManager().registerEvents(new LoginManager(this), this);
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown login
+    /**
+     * The event to run on the player join
+     * @param event
+     */
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
+        long resetTime = getConfig().getLong("ResetTime");
+        String resetWorld = getConfig().getString("ResetWorld");
+
+        if(resetWorld != null) {
+            World world = Bukkit.getWorld(resetWorld);
+            if(world != null && player.getWorld() == world) {
+                if(player.getLastSeen() < resetTime) {
+                    player.teleport(world.getSpawnLocation());
+                }
+            }
+        }
     }
 }
