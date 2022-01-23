cb75a7897ae6c6b6a93a9425e73eaa16dc1f5a51
mov <mov51@mov51.net>
2022-01-23 01:00:18 -0500

    Fix file structure

--- src/main/java/net/mov51/SafeReset/SafeReset.java
+++ src/main/java/net/mov51/SafeReset/SafeReset.java
@@ -4,13 +4,19 @@ import com.earth2me.essentials.Essentials;
 import net.mov51.events.LoginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class SafeEnd extends JavaPlugin{
 
     public Essentials ess = (Essentials) getServer().getPluginManager().getPlugin("Essentials");
 
+    public static Logger logger;
 
     @Override
     public void onEnable() {
+        //getLogger
+        logger = this.getLogger();
+
         // Plugin startup logic
         this.saveDefaultConfig();
         getServer().getPluginManager().registerEvents(new LoginManager(this), this);
